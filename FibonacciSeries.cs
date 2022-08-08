0c10ae4a45d7efd12cf64907e3c9b35a18472f32
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-08 20:18:41 +0530

    Program_to_Print_Fibonacci_Series

---
+++ FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_6_Practice_Problems
+{
+    internal class FibonacciSeries
+    {
+        int firstnumber = 0, secondnumber = 2;
+        int result;
+        public void PrintFibonacci()
+        {
+            Console.WriteLine("Enter a Number to find Fibonacci Series");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i <+ number; i++)
+            {
+                Console.Write(" {0} ", firstnumber);
+                result = firstnumber + secondnumber;
+                firstnumber = secondnumber;
+                secondnumber = result;
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Day_6_Practice_Problems
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            FibonacciSeries fibonacci = new FibonacciSeries();
+            fibonacci.PrintFibonacci();
+        }
+    }
+}
