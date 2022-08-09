66e5353bac2f00d95ab002b5f635ed86fef196e3
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-09 15:45:01 +0530

    Perfect_Number

---
+++ PerfectNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_6_Practice_Problems
+{
+    public class PerfectNumber
+    {
+        int num = 0;
+        int sum = 0;
+        public void Perfect()
+        {
+            Console.WriteLine("Enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < num; i++)
+            {
+                //dividend/divisor=quotient
+                if (num % i == 0)
+                {
+                    Console.WriteLine(i +" ");
+                    sum += i;
+                }
+            }
+            if (sum.Equals(num))
+            {
+                Console.WriteLine("{0} is perfect number", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a perfect number", num);
+            }
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,10 +2,23 @@
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            FibonacciSeries fibonacci = new FibonacciSeries();
-            fibonacci.PrintFibonacci();
+            string a;
+            Console.Write("Enter Program Name : ");
+            a = Convert.ToString(Console.ReadLine());
+            switch(a)
+            {
+                case "FibonacciSeries":
+                    FibonacciSeries fibonacci = new FibonacciSeries();
+                    fibonacci.PrintFibonacci();
+                    break;
+                case "PerfectNumber":
+                    PerfectNumber num = new PerfectNumber();
+                    num.Perfect();
+                    break;
+            }
+            
         }
     }
 }
